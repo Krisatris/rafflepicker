36f53b2b4098227d2d76a49d8c885a93288e3ab0
stella <stella.beemer@gmail.com>
2024-05-12 21:59:01 -0700

    finished script and tested

--- picker.py
+++ picker.py
@@ -17,18 +17,29 @@ def main():
             entrant = row["What is your name/tag (we will call this if you win the raffle so choose carefully!)"]
             score_str = row["Score"]
             score_num = int(score_str[0])
-            for i in score_num:
+            for i in range(score_num):
                 total_entries.append(entrant)
                 i += 1
 
             line_count += 1
 
             if line_count % 10 == 0:
-                print(line_count + " entrants processed. " + total_entries.count() + " entries total.")
+                print(str(line_count) + " entrants processed. " + str(len(total_entries)) + " entries total.")
+    
+    print("all winners processed. there are " + str(len(total_entries)) + " total entries")
 
-    winners = input("Enter num of winners desired: ")
-    for i in winners:
-        r1 = random.randint(0, total_entries.count())
-        print("winner " + i + ": " + total_entries[r1])
+    winners = input("\nEnter num of winners desired: ")
+
+    while winners:
+        winners = int(winners)
+        for i in range(winners):
+            print("\nthere are currently " + str(len(total_entries)) + " total entries")
+            r1 = random.randint(0, len(total_entries))
+            print("winner " + str(i) + ": " + total_entries[r1])
+            claim = input("was the prize claimed? (y/n): ")
+            if claim == 'y':
+                temp_total_entries = [j for j in total_entries if j != total_entries[r1]]
+                total_entries = temp_total_entries
+        winners = input("Enter another number if you need more winners: ")
 
 main()
